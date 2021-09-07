84f13f6e99a5f183bf03620098ae4e6c461b676a
Gullian <gullianvdwalt1@gmail.com>
2021-09-07 07:39:38 +0200

    init

---
+++ src/main/java/com/gvdw/datatables_server_side/DataTablesServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.gvdw.datatables_server_side;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTablesServerSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataTablesServerSideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gvdw/datatables_server_side/DataTablesServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvdw.datatables_server_side;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTablesServerSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
