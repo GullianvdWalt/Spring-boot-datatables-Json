033b81c48d36659cd6cea2aa324190c55dd2e61d
Gullian <gullianvdwalt1@gmail.com>
2021-09-07 09:31:23 +0200

    refactor: services

---
+++ src/main/java/com/gvdw/datatables_server_side/services/ProductServiceImpl.java
@@ -0,0 +1,31 @@
+package com.gvdw.datatables_server_side.services;
+
+import com.gvdw.datatables_server_side.models.Product;
+import com.gvdw.datatables_server_side.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * @author Gullian Van Der Walt
+ * Created at 09:23 on Sep, 2021
+ */
+public class ProductServiceImpl implements ProductService{
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(Integer id) {
+        return productRepository.getById(id);
+    }
+}
