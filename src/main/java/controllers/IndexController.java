111508887b7924c1b938504f5abaedf772b1cc09
Gullian <gullianvdwalt1@gmail.com>
2021-09-07 07:57:13 +0200

    feat: added home controller, bootstrap & Jquery datatable

---
+++ src/main/java/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Gullian Van Der Walt
+ * Created at 07:52 on Sep, 2021
+ */
+@Controller
+public class IndexController {
+    @GetMapping
+    public String mainPage(){
+        return "index";
+    }
+}
